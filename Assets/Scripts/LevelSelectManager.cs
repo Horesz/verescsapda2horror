3e65f60fbae4dbcea0cf411855d5fcc114d35043
Fuleee20 <bukovinszki.balint@gmail.com>
2025-11-04 21:52:34 +0100

    everything fixed

--- Assets/Scripts/LevelSelectManager.cs
+++ Assets/Scripts/LevelSelectManager.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -6,25 +5,12 @@ using UnityEngine.UI;
 public class LevelSelectManager : MonoBehaviour
 {
     public Button[] levelButtons;
-
     private string saveKey = "MaxLevelElert";
 
     void Start()
     {
-        int maxLevelElert = PlayerPrefs.GetInt(saveKey, 1);
-
-        for (int i = 0; i < levelButtons.Length; i++)
-        {
-            if ((i + 1) > maxLevelElert)
-            {
-                levelButtons[i].interactable = false;
-            }
-            else
-            {
-                levelButtons[i].interactable = true;
-            }
-        }
     }
+
     public void LoadLevel(string sceneName)
     {
         if (string.IsNullOrEmpty(sceneName))
@@ -34,8 +20,9 @@ public class LevelSelectManager : MonoBehaviour
 
         SceneManager.LoadScene(sceneName);
     }
+
     public void BackToMenu()
     {
-        SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("Menu");
     }
 }
